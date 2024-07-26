861bf6b332f83d3907816226e76366efcf9c447d
masami10 <masami.nagasawa10@hotmail.com>
2024-07-26 09:52:52 +0800

    feat(xiot):增加了返回信息完整

--- app/xiot_api/crud.py
+++ app/xiot_api/crud.py
@@ -1,13 +1,43 @@
+from typing import List
+
+import sqlalchemy
 from loguru import logger
-from sqlmodel import Field, Session
+from sqlmodel import Field, Session, select, or_
 from app.db import models
 from app.xiot_api import schema
 
 
 def crud_create_tracking_record(db: Session, item: schema.RetraspectsCreate) -> schema.Retraspects:
     rec = schema.Retraspects(jq_sn=item.jq_sn, vendor_sn=item.vendor_sn,
-                             product_code="TBD",product_name='TBD',
+                             product_code="TBD", product_name='TBD',
                              controller_code=item.controller_code, system_code=item.system_code)
+    db.add(rec)
+    db.commit()
+    db.refresh(rec)
+    return rec
+
+
+def crud_existed_records(db: Session, item: schema.RetraspectsCreate) -> schema.Retraspects:
+    statement = select(schema.Retraspects).where(schema.Retraspects.jq_sn == item.jq_sn,
+                                                 schema.Retraspects.system_code == item.system_code,
+                                                 schema.Retraspects.controller_code == item.controller_code,
+                                                 )
+    result = db.exec(statement).one_or_none()
+    return result
+
+
+def crud_conflict_records_existed(db: Session, item: schema.RetraspectsCreate) -> schema.Retraspects:
+    statement = select(schema.Retraspects).where(or_(schema.Retraspects.jq_sn == item.jq_sn,
+                                                     schema.Retraspects.system_code == item.system_code,
+                                                     schema.Retraspects.controller_code == item.controller_code,
+                                                     ))
+    result = db.exec(statement).one_or_none()
+    return result
+
+
+def crud_update_existed_records_vendor_sn(db: Session, rec: sqlalchemy.engine.result,
+                                          vendor_sn: str) -> schema.Retraspects:
+    rec.vendor_sn = vendor_sn
     db.add(rec)
     db.commit()
     db.refresh(rec)
--- app/xiot_api/tracking.py
+++ app/xiot_api/tracking.py
@@ -1,35 +1,107 @@
 from http import HTTPStatus
 from typing import Annotated
-from fastapi import APIRouter, Body, Request, HTTPException
+from fastapi import APIRouter, Body, Request, Response
 from fastapi.responses import JSONResponse
+from loguru import logger
 from sqlmodel import Session
-
+import re
 from app import interface
 from app.exception import OnesphereException
 from app.xiot_api import schema
-from app.xiot_api.crud import crud_create_tracking_record
+from app.xiot_api.crud import crud_create_tracking_record, crud_conflict_records_existed, \
+    crud_update_existed_records_vendor_sn, crud_existed_records
 
 router = APIRouter()
 
 
 @router.post("/retraspects", status_code=HTTPStatus.CREATED, response_model=interface.XtrackingBaseResponse)
-async def create_retraspects(item:  schema.RetraspectsCreate = Body(
-            examples=[
-                {
-                    "jq_sn": "Foo",
-                    "vendor_sn": "A very nice Item",
-                    "system_code": "35.4",
-                    "controller_code": "3.2",
-                }
-            ],
-        ), request: Request = None
-    ):
-    db = request.app.state.db  # 获取默认engine
+async def create_retraspects(item: schema.RetraspectsCreate = Body(
+    examples=[
+        {
+            "jq_sn": "1005737",
+            "vendor_sn": "JQ240123034",
+            "system_code": "000184-70EEEE-EZ01",
+            "controller_code": "ks77h2eggmc0x9t0k36bqnsts6tb2y1268p3r4kngcn3mw4mzaxpd6y5c8qu6hws62wx6w0c8nf akzxz9ap2g37tb43x10q722ko2k0L6Iaavp/aUg+BN/xn0iqRW+ 2rHDR+b77x0ra3BSxMsQBUx1Ug3VN4]CPypeUdf2VBkjqrTZk=yk06ovxovunk4s3lwqunux1cd 7zv9kj7kztobgcm0qm58p6kg08qd927g22522c2hlrscieszhx86ismzgdd5vk1d26f4yy2kpuc",
+        },
+        {
+            "jq_sn": "100573711",
+            "vendor_sn": "JQ240123034",
+            "system_code": "000184-70EEEE-EZ01",
+            "controller_code": "ks77h2eggmc0x9t0k36bqnsts6tb2y1268p3r4kngcn3mw4mzaxpd6y5c8qu6hws62wx6w0c8nf akzxz9ap2g37tb43x10q722ko2k0L6Iaavp/aUg+BN/xn0iqRW+ 2rHDR+b77x0ra3BSxMsQBUx1Ug3VN4]CPypeUdf2VBkjqrTZk=yk06ovxovunk4s3lwqunux1cd 7zv9kj7kztobgcm0qm58p6kg08qd927g22522c2hlrscieszhx86ismzgdd5vk1d26f4yy2kpuc",
+        },
+        {
+            "jq_sn": "1005737",
+            "vendor_sn": "JQ24012303422",
+            "system_code": "000184-70EEEE-EZ01",
+            "controller_code": "ks77h2eggmc0x9t0k36bqnsts6tb2y1268p3r4kngcn3mw4mzaxpd6y5c8qu6hws62wx6w0c8nf akzxz9ap2g37tb43x10q722ko2k0L6Iaavp/aUg+BN/xn0iqRW+ 2rHDR+b77x0ra3BSxMsQBUx1Ug3VN4]CPypeUdf2VBkjqrTZk=yk06ovxovunk4s3lwqunux1cd 7zv9kj7kztobgcm0qm58p6kg08qd927g22522c2hlrscieszhx86ismzgdd5vk1d26f4yy2kpuc",
+        },
+    ],
+), request: Request = None
+):
+    # 首先判断jq_sn有效性
+    if not re.match(r"^\d{7}$", item.jq_sn):
+        response = {
+            "code": HTTPStatus.BAD_REQUEST,
+            "message": "Fail",
+            "data": {"msg": "JQ SN must be 7 digits."},
+        }
+        d = interface.XtrackingBaseResponse(**response)
+        return Response(status_code=HTTPStatus.BAD_REQUEST, content=d.model_dump_json())
+
+    if not re.match(r"^(JQ\d{9}|K\d{11})$", item.vendor_sn):
+        response = {
+            "code": HTTPStatus.BAD_REQUEST,
+            "message": "Fail",
+            "data": {
+                "msg": "Vendor SN must start with 'JQ' followed by 9 digits or 'K' followed by 11 digits."
+            },
+        }
+        d = interface.XtrackingBaseResponse(**response)
+        return Response(status_code=HTTPStatus.BAD_REQUEST, content=d.model_dump_json())
+
     try:
+        db = request.app.state.db  # 获取默认engine
         with Session(db) as session:
+            # 三码同时重复的存在
+            existed_record = crud_existed_records(session, item)
+            if existed_record:
+                logger.info(f'存在完全相同记录: {item.jq_sn}!!!')
+                crud_update_existed_records_vendor_sn(session, existed_record, item.vendor_sn)
+                response = {
+                    "code": HTTPStatus.OK,
+                    "message": "Update a Record Success, Tip: This record database already exists and will be overwritten.",
+                    "data": {
+                        "id": existed_record.id,
+                        "jq_sn": existed_record.jq_sn,
+                        "vendor_sn": existed_record.vendor_sn,
+                        "system_code": existed_record.system_code,
+                        "controller_code": existed_record.controller_code,
+                    },
+                }
+                d = interface.XtrackingBaseResponse(**response)
+                return Response(status_code=HTTPStatus.OK, content=d.model_dump_json())
+
+            # 两码或者一码重复的情况存在
+            conflict_record = crud_conflict_records_existed(session, item)
+            if conflict_record:
+                logger.info(f'存在conflict记录: {item.jq_sn}!!!')
+                response = {
+                    "code": HTTPStatus.CONFLICT,
+                    "message": f"Fail, Cause: system_code or controller_code conflicted with original data in the database, "
+                               f"Please check! Conflict id:{conflict_record.id}",
+                    "data": {
+                        "id": conflict_record.id,
+                        "jq_sn": conflict_record.jq_sn,
+                        "vendor_sn": conflict_record.vendor_sn,
+                        "system_code": conflict_record.system_code,
+                        "controller_code": conflict_record.controller_code,
+                    },
+                }
+                d = interface.XtrackingBaseResponse(**response)
+                return Response(status_code=HTTPStatus.CONFLICT, content=d.model_dump_json())
+
+            # 正常创建记录
             record = crud_create_tracking_record(session, item)
             return interface.XtrackingBaseResponse(data=record)
-            # return JSONResponse(status_code=HTTPStatus.CREATED, content= )
     except Exception as e:
         raise OnesphereException(detail=str(e))
-
