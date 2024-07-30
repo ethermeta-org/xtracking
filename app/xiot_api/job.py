adc997b8895229c46723c8dc249d5a1f06c4cc51
masami10 <masami.nagasawa10@hotmail.com>
2024-07-30 17:25:53 +0800

    chore(job):修复import问题

--- app/xiot_api/job.py
+++ app/xiot_api/job.py
@@ -9,7 +9,7 @@ from app.core.adminsite import scheduler, async_db, source_db, sink_db
 from loguru import logger
 from sqlalchemy import insert, select, update, delete
 from dateutil.relativedelta import relativedelta
-from app.xiot_api.models import SyncLog, MSDelivery, DataTracking
+from app.xiot_api.models import SyncLog, MSDelivery
 from app.config import settings
 from app.xiot_api.schema import Retraspects
 
@@ -36,8 +36,8 @@ async def cron_task_delete_hist_sync_log():
         logger.info(f'删除历史同步日志任务完成。删除日志数量: {r}')
 
 
-@scheduler.scheduled_job('interval', seconds=5)
-# @scheduler.scheduled_job('interval', minutes=sync_interval)
+#@scheduler.scheduled_job('interval', seconds=5)
+@scheduler.scheduled_job('interval', minutes=sync_interval)
 async def cron_task_sync_delivery_records_mssql_to_pg():
     logger.info(f'[EMPOWER]开始同步生产物流信息')
 
