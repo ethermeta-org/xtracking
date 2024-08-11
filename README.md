d06f1e9fd49e414631b73989990d814df7200f4c
masami10 <masami.nagasawa10@hotmail.com>
2024-08-11 18:27:05 +0800

    chore(xiot):更改主题为antd更友好

--- README.md
+++ README.md
@@ -17,3 +17,7 @@ pip install -r ./requirements.txt
 1. 配置文件为config.yaml
 2. 开发环境日志等级为默认DEBUG，不生成日志文件。生产环境生成日志文件，路径: ${workdir}/logs/xtracking.log
 3. 同步日志保存在sqlite数据库中，路径: ${workdir}/data/xtracking.db
+
+
+### *注意*
+1. 前端无法独立打包，所以必须***有网络的环境下***使用
--- app/core/site_settings.py
+++ app/core/site_settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import List
 
 from fastapi_amis_admin.admin.settings import Settings as AmisSettings
+from loguru import logger
 
 from app.utils import check_is_dev
 
@@ -11,6 +12,7 @@ BASE_DIR = os.getcwd()
 
 class Settings(AmisSettings):
     site_title: str = 'Xtracking'
+    version: str = '0.1.0'
     host: str = '0.0.0.0'
     debug: bool = check_is_dev()
     port: int = 8011
@@ -20,7 +22,8 @@ class Settings(AmisSettings):
     # logger = logger
 
 
-site_settings = Settings(database_url_async='sqlite+aiosqlite:///data/database/xtracking.db?check_same_thread=False')
+site_settings = Settings(database_url_async='sqlite+aiosqlite:///data/database/xtracking.db?check_same_thread=False',
+                         amis_theme='antd', logger=logger)
 
 if not os.path.exists(os.path.join(BASE_DIR, 'data', 'database')):
     os.makedirs(os.path.join(BASE_DIR, 'data', 'database'), exist_ok=True)
